52e1dd27db5e4c37c4b6c0e8ba235d1924736f40
LenaUser <eg@digio.ch>
2019-03-12 12:59:56 +0200

    directives-correct

--- src/app/directives/text-style.directive.ts
+++ src/app/directives/text-style.directive.ts
@@ -1,15 +1,17 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appTextStyle]'
 })
 export class TextStyleDirective implements OnInit {
 
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.element.nativeElement.style.lineHeight = '2';
-    this.element.nativeElement.style.fontSize = '16px';
+    const  {nativeElement} = this.element;
+
+    this.renderer.setStyle(nativeElement, 'color', 'blue');
+    this.renderer.addClass(nativeElement, 'text-size');
   }
 
 }
